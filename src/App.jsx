d3142ea18695ef4368e9b58bb6ff1fb42e5a7b92
cheikh tidiane <cheikh951812@gmail.com>
2023-10-10 13:25:42 +0200

    Tp2 : ajout d'éléments graphique

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,87 @@
 import "./css/style.css";
 import "./css/normalize.css";
 import data from "./data/data.json";
-
+import pokemonLogo from "../public/pokImg.svg"
 function App() {
+  function getColorHexa (colorType) {
+    let color ;
 
+    switch (colorType) {
+      case 'Eau':
+        color = 'blue';
+        break;
+
+      case 'Plante':
+        color = 'green';
+        break;
+
+      case 'Poison':
+        color = '#D850C2';
+        break;
+
+      case 'Vol':
+        color = '#738DDB';
+        break;
+
+      case 'Feu':
+        color = 'orange';
+        break;
+
+      case 'Insecte':
+        color = '#70B901';
+        break;
+
+      case 'Électrik':
+        color = '#FFD244';
+        break;
+
+      case 'Sol':
+        color = '#CD793F';
+        break;
+
+      case 'Fée':
+        color = 'pink';
+        break;
+
+      case 'Combat':
+        color = 'darkred';
+        break;
+
+      case 'Psy':
+        color = '#FD6960';
+        break;
+
+      case 'Acier':
+        color = '#246A79';
+        break;
+
+      case 'Glace':
+        color = '#67D1C8';
+        break;
+
+      case 'Roche':
+        color = '#CBB866';
+        break;
+
+      case 'Dragon':
+        color = '#1C6ABB';
+        break;
+
+      default:
+        color = 'grey';
+        break;
+    }
+
+    return color ;
+  }
   return (
     <>
+      <div>
+        <h1><img src={pokemonLogo} alt="Pokemon-Logo" /></h1>
+      </div>
       <main>
       {data.map((pokemon, index)=> (
-      <article key={'Pokemon'+ index}>
+      <article key={'Pokemon'+ index} style={{backgroundColor: getColorHexa(pokemon.apiTypes[0].name) , borderColor: getColorHexa(pokemon.apiTypes[0].name) }}>
         <figure>
           <picture>
             <img src={pokemon.image} alt={pokemon.name} />
