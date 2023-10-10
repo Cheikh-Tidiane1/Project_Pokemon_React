3e403d9a9ba14ffa79de246a87d796d8d973a303
cheikh tidiane <cheikh951812@gmail.com>
2023-10-10 12:50:35 +0200

    Tp1 : Création d'une liste avec React

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+// import './App.css'
 import "./css/style.css";
 import "./css/normalize.css";
 import data from "./data/data.json";
@@ -7,7 +7,29 @@ function App() {
 
   return (
     <>
-      <h1>Hello dworld !</h1> 
+      <main>
+      {data.map((pokemon, index)=> (
+      <article key={'Pokemon'+ index}>
+        <figure>
+          <picture>
+            <img src={pokemon.image} alt={pokemon.name} />
+          </picture>
+          <figcaption>
+            <span className='types'>{pokemon.apiTypes[0].name}</span>
+            <h2>{pokemon.name}</h2>
+            <ol>
+              <li>Points de vie : {pokemon.stats.HP}</li>
+              <li>Attaque : {pokemon.stats.attack}</li>
+              <li>Défense : {pokemon.stats.defense}</li>
+              <li>Attaque spécial : {pokemon.stats.special_attack}</li>
+              <li>Vitesse : {pokemon.stats.speed}</li>
+            </ol>
+          </figcaption>
+        </figure>
+      </article>
+      ))
+      }
+      </main>
     </>
   )
 }
