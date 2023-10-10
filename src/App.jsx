4df0e4767e5d0282eec27c8b986fa5b1cdba8935
cheikh tidiane <cheikh951812@gmail.com>
2023-10-10 18:12:05 +0200

    Tp5 : utilisation de usestate

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,23 @@
 import "./css/style.css";
 import "./css/normalize.css";
 import data from "./data/data.json";
-import pokemonLogo from "../public/pokImg.svg"
+import pokemonLogo from "../public/pokImg.svg";
 import CardPokemon from "./Components/CardPokemon";
 function App() {
   return (
     <>
       <div>
-        <h1><img src={pokemonLogo} alt="Pokemon-Logo" /></h1>
+        <h1>
+          <img src={pokemonLogo} alt="Pokemon-Logo" />
+        </h1>
       </div>
       <main>
-        <CardPokemon data={data}/>
+        {data.map((pokemon, index) => (
+            <CardPokemon data={pokemon} key={index}/>
+          ))}
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/CardPokemon.jsx
+++ src/Components/CardPokemon.jsx
@@ -1,115 +1,125 @@
+import { useState } from "react";
 import "./CardPokemon.scss";
-import "../css/style.css"
-import "../css/normalize.css"
+import "../css/style.css";
+import "../css/normalize.css";
 
-function CardPokemon (props) {
-    return(
-    <>
-    {props.data.map((pokemon, index)=> (
-      <article key={'Pokemon'+ index} id="pokeArticle" data-reverse={false} onClick={(e)=>{
-            e.currentTarget.dataset.reverse = true ;
-      }}>
-        <div className="cardInner"
-            style={{backgroundColor: getColorHexa(pokemon.apiTypes[0].name) ,
-            borderColor: getColorHexa(pokemon.apiTypes[0].name) }}>
-        <figure className="cardFront">
-          <picture>
-            <img src={pokemon.image} alt={pokemon.name} />
-          </picture>
-          <figcaption>
-            <span className='types'>{pokemon.apiTypes[0].name}</span>
-            <h2>{pokemon.name}</h2>
-            <ol>
-              <li>Points de vie : {pokemon.stats.HP}</li>
-              <li>Attaque : {pokemon.stats.attack}</li>
-              <li>Défense : {pokemon.stats.defense}</li>
-              <li>Attaque spécial : {pokemon.stats.special_attack}</li>
-              <li>Vitesse : {pokemon.stats.speed}</li>
-            </ol>
-          </figcaption>
-        </figure>
-        <div className="cardBack">
-          <a href="#">Voir plus</a>
-        </div>
-        </div>
-      </article>
-      ))
-      }
-    </>
-    )
+export default function CardPokemon(props) {
+    const [isOpen, setIsOpen] = useState(Boolean)
+    let pokemon = props.data
+  return (
+        <article
+          key={"Pokemon" + props.index}
+          id="pokeArticle"
+          data-reverse={isOpen}
+          onClick={(e) => {
+            if(isOpen === false){
+                setIsOpen(true)
+              }else{
+                setIsOpen(false)
+              }
+          }}
+        >
+          <div
+            className="cardInner"
+            style={{
+              backgroundColor: getColorHexa(pokemon.apiTypes[0].name),
+              borderColor: getColorHexa(pokemon.apiTypes[0].name),
+            }}
+          >
+            <figure className="cardFront">
+              <picture>
+                <img src={pokemon.image} alt={pokemon.name} />
+              </picture>
+              <figcaption>
+                <span className="types">{pokemon.apiTypes[0].name}</span>
+                <h2>{pokemon.name}</h2>
+                <ol>
+                  <li>Points de vie : {pokemon.stats.HP}</li>
+                  <li>Attaque : {pokemon.stats.attack}</li>
+                  <li>Défense : {pokemon.stats.defense}</li>
+                  <li>Attaque spécial : {pokemon.stats.special_attack}</li>
+                  <li>Vitesse : {pokemon.stats.speed}</li>
+                </ol>
+              </figcaption>
+            </figure>
+            <div className="cardBack">
+              <a href="#">Voir plus</a>
+            </div>
+          </div>
+        </article>
+  );
 }
 
-export default CardPokemon ;
 
 
-function getColorHexa (colorType) {
-    let color ;
+function getColorHexa(colorType) {
+  let color;
 
-    switch (colorType) {
-      case 'Eau':
-        color = 'blue';
-        break;
+  switch (colorType) {
+    case "Eau":
+      color = "blue";
+      break;
 
-      case 'Plante':
-        color = 'green';
-        break;
+    case "Plante":
+      color = "green";
+      break;
 
-      case 'Poison':
-        color = '#D850C2';
-        break;
+    case "Poison":
+      color = "#D850C2";
+      break;
 
-      case 'Vol':
-        color = '#738DDB';
-        break;
+    case "Vol":
+      color = "#738DDB";
+      break;
 
-      case 'Feu':
-        color = 'orange';
-        break;
+    case "Feu":
+      color = "orange";
+      break;
 
-      case 'Insecte':
-        color = '#70B901';
-        break;
+    case "Insecte":
+      color = "#70B901";
+      break;
 
-      case 'Électrik':
-        color = '#FFD244';
-        break;
+    case "Électrik":
+      color = "#FFD244";
+      break;
 
-      case 'Sol':
-        color = '#CD793F';
-        break;
+    case "Sol":
+      color = "#CD793F";
+      break;
 
-      case 'Fée':
-        color = 'pink';
-        break;
+    case "Fée":
+      color = "pink";
+      break;
 
-      case 'Combat':
-        color = 'darkred';
-        break;
+    case "Combat":
+      color = "darkred";
+      break;
 
-      case 'Psy':
-        color = '#FD6960';
-        break;
+    case "Psy":
+      color = "#FD6960";
+      break;
 
-      case 'Acier':
-        color = '#246A79';
-        break;
+    case "Acier":
+      color = "#246A79";
+      break;
 
-      case 'Glace':
-        color = '#67D1C8';
-        break;
+    case "Glace":
+      color = "#67D1C8";
+      break;
 
-      case 'Roche':
-        color = '#CBB866';
-        break;
+    case "Roche":
+      color = "#CBB866";
+      break;
 
-      case 'Dragon':
-        color = '#1C6ABB';
-        break;
+    case "Dragon":
+      color = "#1C6ABB";
+      break;
 
-      default:
-        color = 'grey';
-        break;
-    }
-
-    return color ;
+    default:
+      color = "grey";
+      break;
   }
+
+  return color;
+}
