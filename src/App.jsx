c836dd2ff56e483e14bcd708a65f691e2ad9dca4
cheikh tidiane <cheikh951812@gmail.com>
2023-10-10 11:56:12 +0200

    Initialisation du projet

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import "./css/style.css";
+import "./css/normalize.css";
+import data from "./data/data.json";
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello dworld !</h1> 
+    </>
+  )
+}
+
+export default App
