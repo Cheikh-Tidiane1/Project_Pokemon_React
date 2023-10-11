e5a2f658b17d592610c2f9a0b496399cdfae8b45
cheikh tidiane <cheikh951812@gmail.com>
2023-10-11 18:19:08 +0200

    Tp6 : ouverture sidebar à l'aide des states

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,22 @@ import "./css/normalize.css";
 import data from "./data/data.json";
 import pokemonLogo from "../public/pokImg.svg";
 import CardPokemon from "./Components/CardPokemon";
+import Sidebar from "./Components/Sidebar";
 function App() {
   return (
     <>
-      <div>
+      <div className="header">
         <h1>
           <img src={pokemonLogo} alt="Pokemon-Logo" />
         </h1>
       </div>
       <main>
-        {data.map((pokemon, index) => (
-            <CardPokemon data={pokemon} key={index}/>
-          ))}
+      <div id="sideBar"> 
+        <Sidebar/>
+      </div>
+      {data.map((pokemon, index) => (
+          <CardPokemon data={pokemon} key={index}/>
+        ))}
       </main>
     </>
   );
